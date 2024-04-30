933ba12ed531296a69d8957a7fab747425280849
kevinanewman <newman.kevin@epa.gov>
2024-04-30 08:40:00 -0400

    Bump version: 0.0.2 → 0.0.3

--- versioning/build_exe_dist_win.bat
+++ versioning/build_exe_dist_win.bat
@@ -5,7 +5,7 @@ cd ..
 REM build executable
 
 pyinstaller exe_entry.py ^
-    --name colliderscope2024-0.0.2-win ^
+    --name colliderscope2024-0.0.3-win ^
     --paths .;filterwidget ^
     --add-data assets;assets ^
     --noconfirm ^
