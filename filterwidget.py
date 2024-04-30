b3e5bbe4ae1430a23ee07f077578613be58e82a6
kevinnewman <newman.kevin@epa.gov>
2024-04-30 09:02:29 -0400

    bugfix for trying to filter with an empty text edit

--- filterwidget.py
+++ filterwidget.py
@@ -21,44 +21,45 @@ class FilterWidget(QWidget):
         self.widget_list = widget_list
 
     def inputChanged(self):
-        if self.ui.not_pushButton.isChecked():
-            invert = 'not'
-        else:
-            invert = ''
-
-        if (self.ui.cc_pushButton.isChecked() or
-                (self.ui.comboBox.currentText() == 'contains' and self.ui.w_pushButton.isChecked())):
-            case_modifier = ''
-        else:
-            case_modifier = '.lower()'
-
-        if self.ui.w_pushButton.isChecked():
-            compare = '=='
-        else:
-            compare = 'in'
-
-        for lw in self.widget_list:
-            # print(lw.source_data)
-            lw.clear()
-
-            if self.ui.comboBox.currentText() == 'contains':
-                string_fun = ''
-                self.ui.w_pushButton.setEnabled(True)
-            elif self.ui.comboBox.currentText() == 'starts with':
-                string_fun = '.startswith'
-                self.ui.w_pushButton.setEnabled(False)
-            else:  # self.ui.comboBox.currentText() == 'ends with':
-                string_fun = '.endswith'
-                self.ui.w_pushButton.setEnabled(False)
-
-            if string_fun:
-                fields = eval(
-                    f'[f for f in lw.source_data if {invert} f{case_modifier}{string_fun}(self.ui.lineEdit.text(){case_modifier})]'.format(), locals())
+        if self.ui.lineEdit.text():
+            if self.ui.not_pushButton.isChecked():
+                invert = 'not'
             else:
-                fields = eval(
-                    f'[f for f in lw.source_data if {invert} (self.ui.lineEdit.text(){case_modifier} {compare} f{case_modifier})]'.format(), locals())
+                invert = ''
 
-            lw.addItems(fields)
+            if (self.ui.cc_pushButton.isChecked() or
+                    (self.ui.comboBox.currentText() == 'contains' and self.ui.w_pushButton.isChecked())):
+                case_modifier = ''
+            else:
+                case_modifier = '.lower()'
+
+            if self.ui.w_pushButton.isChecked():
+                compare = '=='
+            else:
+                compare = 'in'
+
+            for lw in self.widget_list:
+                # print(lw.source_data)
+                lw.clear()
+
+                if self.ui.comboBox.currentText() == 'contains':
+                    string_fun = ''
+                    self.ui.w_pushButton.setEnabled(True)
+                elif self.ui.comboBox.currentText() == 'starts with':
+                    string_fun = '.startswith'
+                    self.ui.w_pushButton.setEnabled(False)
+                else:  # self.ui.comboBox.currentText() == 'ends with':
+                    string_fun = '.endswith'
+                    self.ui.w_pushButton.setEnabled(False)
+
+                if string_fun:
+                    fields = eval(
+                        f'[f for f in lw.source_data if {invert} f{case_modifier}{string_fun}(self.ui.lineEdit.text(){case_modifier})]'.format(), locals())
+                else:
+                    fields = eval(
+                        f'[f for f in lw.source_data if {invert} (self.ui.lineEdit.text(){case_modifier} {compare} f{case_modifier})]'.format(), locals())
+
+                lw.addItems(fields)
 
     def goto_settings(self):
         self.ui.stackedWidget.setCurrentIndex(1)
