2ef183e0c7bb1e9996456e2ecb8f9f523d4f708e
kevinanewman <newman.kevin@epa.gov>
2024-05-17 17:13:57 -0400

    Bump version: 0.0.22 → 0.0.23

--- README.rst
+++ README.rst
@@ -1,6 +1,6 @@
 ColliderScope2024
 =================
 
-Version 0.0.22
+Version 0.0.23
 
 Like the original colliderscope, only better!
--- versioning/build_exe_dist_win.bat
+++ versioning/build_exe_dist_win.bat
@@ -5,7 +5,7 @@ cd ..
 REM build executable
 
 pyinstaller exe_entry.py ^
-    --name colliderscope2024-0.0.22-win ^
+    --name colliderscope2024-0.0.23-win ^
     --paths .;filterwidget ^
     --add-data assets;assets ^
     --noconfirm ^
