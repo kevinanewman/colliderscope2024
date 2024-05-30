caf0eae29f66a82d1487391e6758a429f712a151
kevinanewman <newman.kevin@epa.gov>
2024-05-30 09:30:43 -0400

    Bump version: 0.0.27 → 0.0.28

--- README.rst
+++ README.rst
@@ -1,6 +1,6 @@
 ColliderScope2024
 =================
 
-Version 0.0.27
+Version 0.0.28
 
 Like the original colliderscope, only better!
--- __init__.py
+++ __init__.py
@@ -2,7 +2,7 @@ import pandas as pd
 
 print('\n--- Colliderscope executable launching, this may take a few moments ---\n')
 
-code_version = '0.0.27'
+code_version = '0.0.28'
 
 
 def print_dict(dict_in, num_tabs=0, to_string=False):
